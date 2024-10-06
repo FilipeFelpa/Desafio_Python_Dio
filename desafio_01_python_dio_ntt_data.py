d0d5e9a0b7da037daa623ff1ef730dac23b7c28c
Filipe Felpa <filipefelpa@hotmail.com.br>
2024-10-06 18:40:19 -0400

    Update desafio_01_python_dio_ntt_data.py
    Atualização do desafio com a correção utilizando funções

--- desafio_01_python_dio_ntt_data.py
+++ desafio_01_python_dio_ntt_data.py
@@ -11,76 +11,50 @@ def menu():
     [2]\tSaque
     [3]\tExtrato
     [4]\tSair
-    O que seja realiza => """
+    O que deseja realizar => """
     return int(input(menu))
 
-#def deposito(saldo1):
-#    saldo_temp = saldo1
-#    valor_depositado = float(input("Informe o valor a ser depositado: "))
-#    if valor_depositado > 0:
-#        saldo_temp = saldo_temp + valor_depositado
-#        global extrato += f"Depósito: R$ {valor_depositado:.2f}\n"
-#        return saldo_temp
-#    else:
-#        print("Valor informado é invalido, favor preencher com um valor valido")
-#        menu()
-    
-#def saque(saldo,quant_limite_saque,limite):
-#    valor = float(input("Quanto deseja sacar?: "))
-#    passou_limite = valor > limite
-#    passou_saldo = valor > saldo
-#    limite_saque = quant_limite_saque == 0
+def deposito(saldo, extrato):
+    valor_depositado = float(input("Informe o valor a ser depositado: "))
+    if valor_depositado > 0:
+        saldo += valor_depositado
+        extrato += f"Depósito: R$ {valor_depositado:.2f}\n"
+        return saldo, extrato
+    else:
+        print("Valor informado é inválido, favor preencher com um valor válido.")
+        return saldo, extrato
 
-#    if passou_saldo:
-#        print("Saldo insuficiente!")
-#    elif passou_limite:
-#        print("Solicitado acima do limite permitido! Tente novamente")
-#    elif limite_saque:
-#        print("Foi excedido se limite de saque diário!")
-#    elif valor > 0:
-#        saldo -= valor
-#        global extrato += f"Saque: R$ {valor:.2f}\n"
-#        return saldo
-#    else:
-#        print("OPeração não reconhecida!")
+def saque(saldo, quant_limite_saque, limite, extrato):
+    valor = float(input("Quanto deseja sacar?: "))
+    passou_limite = valor > limite
+    passou_saldo = valor > saldo
+    limite_saque = quant_limite_saque == 0
+
+    if passou_saldo:
+        print("Saldo insuficiente!")
+    elif passou_limite:
+        print("Solicitado acima do limite permitido! Tente novamente.")
+    elif limite_saque:
+        print("Foi excedido seu limite de saques diário!")
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        return saldo, quant_limite_saque - 1, extrato
+    else:
+        print("Operação não reconhecida!")
+        return saldo, quant_limite_saque, extrato
 
 quant_limite_saque = 3
 limite = 500
-saldo = 0.0
+saldo = 0
 extrato = ""
 
 while True:
     resultado = menu()
     if resultado == 1:
-#       saldo = deposito(saldo)
-        valor_depositado = float(input("Informe o valor a ser depositado: "))
-        if valor_depositado > 0:
-            saldo += valor_depositado
-            extrato += f"Depósito: R$ {valor_depositado:.2f}\n"
-            print("Deposito realizado!")
-        else:
-            print("Valor informado é invalido, favor preencher com um valor valido")
+        saldo, extrato = deposito(saldo, extrato)
     elif resultado == 2:
-#        saldo = saque(saldo,quant_limite_saque,limite)
-#        quant_limite_saque -= 1
-        valor = float(input("Quanto deseja sacar?: "))
-        passou_limite = valor > limite
-        passou_saldo = valor > saldo
-        limite_saque = quant_limite_saque == 0
-
-        if passou_saldo:
-            print("Saldo insuficiente!")
-        elif passou_limite:
-            print("Solicitado acima do limite permitido! Tente novamente")
-        elif limite_saque:
-            print("Foi excedido se limite de saque diário!")
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            print("Saque realizado")
-            quant_limite_saque -= 1
-        else:
-            print("OPeração não reconhecida!")
+        saldo, quant_limite_saque, extrato = saque(saldo, quant_limite_saque, limite, extrato)
     elif resultado == 3:
         print("\n================ EXTRATO ================")
         print("Não foram realizadas movimentações." if not extrato else extrato)
@@ -90,3 +64,4 @@ while True:
         break
     else:
         print("Operação inválida, tente novamente!")
+
